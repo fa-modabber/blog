c3efe7a3ffde89e03edf5826ae6bec3145ceee11
Fatemeh Modabber <fateme.modabber.73@gmail.com>
2025-03-08 15:31:13 +0330

    some changes

---
+++ blog/include/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("SERVERNAME","localhost");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_NAME","php_course_blog");
+define("DNS","mysql:host=".SERVERNAME.";dbname=".DB_NAME.";charset=utf8mb4");
--- blog/include/layout/navbar.php
+++ blog/include/layout/navbar.php
@@ -1,13 +1,34 @@
+<?php
+
+include "./include/config.php";
+include "./include/db.php";
+
+try {
+
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "SELECT * FROM categories";
+    $categories = $db->query($query);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
+
 <body>
     <div class="container">
         <section class="navbar-section">
             <div class="top-nav d-flex flex-column flex-md-row justify-content-md-between align-items-center py-3">
                 <a href="" class="brand fw-bold">My Weblog</a>
                 <nav>
-                    <a href="" class="fw-bold">nature</a>
-                    <a href="">tourism</a>
-                    <a href="">tech</a>
-                    <a href="">other</a>
+                    <?php if ($categories->rowCount() > 0): ?>
+                        <?php foreach ($categories as $category): ?>
+                            <a href="" class="fw-bold">
+                                <?= $category['title'] ?>
+                            </a>
+                        <?php endforeach ?>
+                    <?php endif ?>
                 </nav>
             </div>
             <hr>
