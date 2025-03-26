82f9662d6322d5852934fbf12b75931e0750f8de
Fatemeh Modabber <fateme.modabber.73@gmail.com>
2025-03-26 16:19:13 +0330

    working on comments

--- blog/single-post.php
+++ blog/single-post.php
@@ -6,15 +6,18 @@ include "./layout/includes/navbar.php";
 
 if (isset($_GET['post'])) {
     $postId = $_GET['post'];
-    $query = "SELECT * FROM posts WHERE id=:id";
-    $post = $db->prepare($query);
+    $post = $db->prepare("SELECT * FROM posts WHERE id=:id");
     $post->execute(['id' => $postId]);
-    $post = $post->fetch(PDO::FETCH_ASSOC);
+    $post = $post->fetch();
+
     $categoryId = $post['category_id'];
     $postCategory = $db->query("SELECT * FROM categories WHERE id=$categoryId")->fetch();
     $userId = $post['user_id'];
     $user = $db->query("SELECT * FROM users WHERE id=$userId")->fetch();
     $userFullName = $user['first_name'] . " " . $user['last_name'];
+
+    $comments = $db->prepare("SELECT * FROM comments WHERE post_id=:id AND is_accepted='1'");
+    $comments->execute(['id' => $postId]);
 }
 
 ?>
@@ -22,41 +25,77 @@ if (isset($_GET['post'])) {
 <section class="content mt-4">
     <div class="row">
         <div class="col-lg-8">
-
-            <?php if ($post->rowCount() == 0): ?>
+            <?php if (empty($post)): ?>
                 <div class="alert alert-danger" role="alert">
                     no post found!
                 </div>
             <?php else: ?>
-                <div class="single-post">
-                    <div>
-                        <img src="../uploads/posts/<?= $post['image'] ?>" class="img-fluid rounded" alt="post image">
+                <!-- single post -->
+                <div class="card">
+                    <img src="../uploads/posts/<?= $post['image'] ?>" class="card-img-top" alt="post image">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <h5 class="card-title">
+                                <?= $post['title'] ?>
+                            </h5>
+                            <div><span class="badge text-bg-secondary">
+                                    <?= $postCategory['title'] ?>
+                                </span></div>
+                        </div>
+                        <p class="text-justify">
+                            <?= $post['body'] ?>
+                        </p>
+                        <div>
+                            <p class="mb-0 fw-semibold">writer: <?= $userFullName ?></p>
+                        </div>
                     </div>
-                    <div class="d-flex justify-content-between">
-                        <h5 class="card-title">
-                            <?= $post['title'] ?>
-                        </h5>
-                        <div><span class="badge text-bg-secondary">
-                                <?= $postCategory['title'] ?>
-                            </span></div>
-                    </div>
-                    <div>
-                        <?= $post['body'] ?>
-                    </div>
-                    <p class="mb-0">writer: <?= $userFullName ?></p>
                 </div>
+
+                <hr class="bg-secondary border-2 border-top border-secondary" />
+
+                <!-- comment form -->
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Write a Comment</h5>
+                        <form method="post" action="">
+                            <div class="mb-3">
+                                <label for="exampleInputName1" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="exampleInputName1" aria-label="name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            </div>
+                            <button type="submit" class="btn btn-dark">Submit</button>
+                        </form>
+                    </div>
+                </div>
+
+                <!-- list of comments -->
+                <p class="fw-bold fs-6">Number of Comments: <?= $comments->rowCount() ?></p>
+                <?php if ($comments->rowCount() > 0): ?>
+                    <?php foreach ($comments as $comment):  ?>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?= $comment['name'] ?>
+                                </h5>
+                                <p class="card-text"><?= $comment['body'] ?></p>
+                            </div>
+                        </div>
+                    <?php endforeach;  ?>
+                <?php endif; ?>
             <?php endif; ?>
         </div>
+        <!-- sidebar -->
+        <?php
+        include "./layout/includes/sidebar.php";
+        ?>
     </div>
 </section>
 
 
-<!-- sidebar -->
-<?php
-include "./layout/includes/sidebar.php";
-?>
-</div>
-</section>
+
 
 <?php
 include "./layout/includes/footer.php";
